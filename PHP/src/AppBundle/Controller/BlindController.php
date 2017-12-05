3a5dfa7f95dd4808d401baa4f632ed63bd006dbb
eker247 <eker247@wp.pl>
2017-12-05 13:01:16 +0100

    dodanie kategorii rolety, wstawione tła

---
+++ PHP/src/AppBundle/Controller/BlindController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Utils\LightDriver;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+
+class BlindController extends Controller
+{
+    private $driver;
+    public function __construct()
+    {
+        $this->driver = new LightDriver();
+    }
+
+    /**
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function indexAction(Request $request)
+    {
+        $rooms = $this->driver->LampStatuses();
+        return $this->render('blind/index.html.twig', ['rooms' => $rooms]);
+    }
+
+    /**
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function transmitterAction($tid = 0)
+    {
+        try {
+            $this->driver->SwitchLight($tid);
+        }
+        catch (Exception $ex) {
+            return new Response($ex->getMessage());
+        }
+        return $this->redirectToRoute('light_index');
+    }
+}
